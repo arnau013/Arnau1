e1e0ce54553f3d93182ffbb20ba930005de37229
arnau013 <149940526+arnau013@users.noreply.github.com>
2025-01-31 13:35:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,10 @@ noButton.addEventListener("click", function() {
         message.textContent = "Va, dona-li que sí, no siguis aixi!";
     } else if (clickCount == 3) {
         message.textContent = "Si prems 'No' una altra vegada, morirà un monito 🐵";
+    } else if (clickCount == 4) {
+        message.textContent = "Alaaaaaa pobre monito 🐒, arregleu amb un SI"";
     } else {
-        message.textContent = "Alaaaaaa pobre monito 🐒, arregleu amb un SI";
+        message.textContent = "Doncs no me queda otra...;
+        yesButton.style.fontSize = 0.001;
     }
 });
