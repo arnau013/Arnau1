2d6217ae0121d07cee79ed1817601495b5401569
arnau013 <149940526+arnau013@users.noreply.github.com>
2025-01-31 11:33:42 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("botonRojo").addEventListener("click", function() {
+    alert("Has presionado el botón rojo");
+});
+
+document.getElementById("botonAzul").addEventListener("click", function() {
+    alert("Has presionado el botón azul");
+});
