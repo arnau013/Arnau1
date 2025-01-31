6cdd3d45d4eb0b27afbdcd535333a164ea2d3d2e
arnau013 <149940526+arnau013@users.noreply.github.com>
2025-01-31 11:48:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-document.getElementById("botonRojo").addEventListener("click", function() {
-    alert("Has presionado el botón rojo");
+let noButton = document.getElementById("no");
+let yesButton = document.getElementById("yes");
+let message = document.getElementById("message");
+let size = 18;
+let clickCount = 0;
+
+yesButton.addEventListener("click", function() {
+    message.textContent = "Gràcies ratona, tk!";
 });
 
-document.getElementById("botonAzul").addEventListener("click", function() {
-    alert("Has presionado el botón azul");
+noButton.addEventListener("click", function() {
+    clickCount++;
+    size += 10;
+    yesButton.style.fontSize = size + "px";
+    
+    if (clickCount == 1) {
+        message.textContent = "Segura?";
+    } else if (clickCount == 2) {
+        message.textContent = "Va, dona-li que sí, no siguis dolenta!";
+    } else if (clickCount == 3) {
+        message.textContent = "Si prems 'No' una altra vegada, ploraré 😢";
+    } else {
+        message.textContent = "Ja estic plorant... ara ho arregles! 😭";
+    }
 });
